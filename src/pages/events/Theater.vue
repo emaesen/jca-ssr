782971e109ee2657ff78c77daff08dfb76054a7d
Edward <edward.maesen@gmail.com>
2019-10-11 16:00:12 -0700

    Port from jca

---
+++ src/pages/events/Theater.vue
@@ -0,0 +1,51 @@
+<template>
+  <Layout>
+    <h1>Theater at JCA</h1>
+
+    <p>
+      <cite>
+        "A stage play ought to be the point of intersection between the visible and invisible worlds”
+      </cite>
+      ~Arthur Adamov
+    </p>
+    <p>
+      Jefferson Center for the Arts is the home for original works as well as traveling productions, plays, musicals, melodramas, children’s theatre and more. 
+    </p>
+    <p>
+      We are currently planning our opening season. 
+      Stay tuned!
+    </p>
+
+    <img class="img-hero anima__zoom" src="/img/temp/kyle-head-p6rNTdAPbuk-unsplash.jpg" alt="theater category"/>
+
+    <event-list category="theater"/>
+
+  </Layout>
+</template>
+
+<script>
+import EventList from '@/components/EventList';
+
+export default {
+  name: 'Theater',
+  components: {
+    EventList,
+  },
+  metaInfo: {
+    title: "Theater Events",
+    meta: [
+      { name: "description", content: "An overview of upcoming theater events at Jefferson Center for the Arts in Mount Shasta, CA, USA." }
+    ]
+  },  
+  data() {
+    return {
+    }
+  }
+};
+</script>
+
+<style lang="less" scoped>
+.quote {
+  display: block;
+}
+</style>
---
+++ src/pages/events/VisualArts.vue
@@ -0,0 +1,43 @@
+<template>
+  <Layout>
+    <h1>Visual Arts at JCA</h1>
+
+    <p>
+      Jefferson Center for the Arts presents a wide-variety of rotating exhibitions featuring works by international and regional artists.
+      Artists chosen for our gallery space will be encouraged to share their methods and inspirations during gallery openings and workshops.
+    </p>
+    <p>
+      Check back for Show Entry Applications for our opening season!
+    </p>
+
+    <img class="img-hero anima__zoom" src="/img/temp/khara-woods-KR84RpMCb0w-unsplash.jpg" alt="visual-arts category"/>
+
+    <event-list category="visual-arts"/>
+
+  </Layout>
+</template>
+
+<script>
+import EventList from '@/components/EventList';
+
+export default {
+  name: 'VisualArts',
+  components: {
+    EventList,
+  },
+  metaInfo: {
+    title: "Visual Arts Events",
+    meta: [
+      { name: "description", content: "An overview of upcoming visual arts events at Jefferson Center for the Arts in Mount Shasta, CA, USA." }
+    ]
+  },  
+  data() {
+    return {
+    }
+  }
+};
+</script>
+
+<style lang="less" scoped>
+
+</style>
