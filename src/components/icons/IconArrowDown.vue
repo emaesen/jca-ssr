fea4197a1af62b758ebcc7ab8211b521f0d09814
Edward <edward.maesen@gmail.com>
2019-10-11 14:33:23 -0700

    Port from jca - svg icons

---
+++ src/components/icons/IconArrowDown.vue
@@ -0,0 +1,12 @@
+<template>
+  <svg version="1.1" role="presentation" width="9" height="14.4" viewBox="0 0 320 512" class="icon icon-arrowdown">
+    <path d="M31.3 192H288.6C306.4 192 315.3 213.5 302.7 226.1L174.1 354.8C166.3 362.6 153.6 362.6 145.8 354.8L17.2 226.1C4.6 213.5 13.5 192 31.3 192z"></path>
+    <!---->
+  </svg>
+</template>
+
+<script>
+export default {
+  name: 'IconArrowDown'
+}
+</script>
