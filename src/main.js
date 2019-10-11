aa83037d6d880d7a4ba9469be5ef4c8cbf4a6a9e
Edward <edward.maesen@gmail.com>
2019-10-11 16:12:43 -0700

    Port store settings from jca

--- src/main.js
+++ src/main.js
@@ -9,9 +9,44 @@ export default function (Vue, { router, head, isClient, appOptions }) {
   Vue.component('Layout', DefaultLayout);
   Vue.use(Vuex);
   appOptions.store = new Vuex.Store({
-    state:{
+    state: {
+      timeTick: new Date().getTime(),
+      calendar: {
+        today: null,
+        month: {
+          start: null,
+          end: null
+        },
+        dayInFocus: null,
+        dateRange: {
+          start: null,
+          end: null
+        }
+      },
+    },
+    getters: {
+      timeTick: state => state.timeTick,
+      calendar: state => state.calendar
     },
     mutations: {
-    }
+      // can be used directly for synchronous events
+      SET_TIME_TICK: (state, tick) => (state.timeTick = tick),
+      SET_CALENDAR_MONTH: (state, payload) => {
+        state.calendar.month.start = payload.start;
+        state.calendar.month.end = payload.end;
+      },
+      SET_CALENDAR_DATERANGE: (state, payload) => {
+        state.calendar.dateRange.start = payload.start;
+        state.calendar.dateRange.end = payload.end;
+      },
+      SET_CALENDAR_DAYINFOCUS: (state, day) => {
+        state.calendar.dayInFocus = day;
+      },
+      SET_CALENDAR_TODAY: (state, day) => {
+        state.calendar.today = day;
+      },
+    },
+    actions: {
+    }  
   });
 }
