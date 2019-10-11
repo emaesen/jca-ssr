36c126b59265750fe5ced4dcf1a8aa120d1abf77
Edward <edward.maesen@gmail.com>
2019-10-11 15:25:13 -0700

    Port from jca

---
+++ src/pages/404.vue
@@ -0,0 +1,43 @@
+<template>
+  <Layout>
+    <div class="center anima__zoom pnf">
+      <p class="anima__flicker">P A G E &nbsp; &nbsp; N O T &nbsp; &nbsp; F O U N D</p>
+      <p> - - - - - </p>
+      <p class="anima__fade-in-bottom delay1">S O</p>
+      <p class="anima__fade-in-bottom delay2">S O R R Y</p>
+      <p class="anima__fade-in-bottom delay3 spacious">
+        The page you are looking for does not –or no longer– exist.<br>
+        Please click any of the links in the navigation bar above to continue.<br>
+      </p>
+    </div>
+  </Layout>
+</template>
+
+
+<script>
+export default {
+  name: '404',
+  metaInfo: {
+    title: "Page Not Found",
+    meta: [
+      { name: "description", content: "Page Not Found" }
+    ]
+  }
+}
+</script>
+
+
+<style lang="less" scoped>
+.pnf {
+  margin: 8em 0 30em 0;
+}
+.delay1 {
+  animation-delay: 1s;
+}
+.delay2 {
+  animation-delay: 1.1s;
+}
+.delay3 {
+  animation-delay: 2s;
+}
+</style>
