df39081be81454127877346ba17cbb900ac22eaa
Edward <edward.maesen@gmail.com>
2019-10-15 11:19:58 -0700

    Add event page endpoints

--- gridsome.server.js
+++ gridsome.server.js
@@ -6,11 +6,54 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = function (api) {
-  api.loadSource(({ addCollection }) => {
+  api.loadSource(async ({ addCollection }) => {
     // Use the Data Store API here: https://gridsome.org/docs/data-store-api/
+    const eventsCollection = addCollection('Event');
+    const eventsJson = await require('./src/data/events.json');
+    for(const item of eventsJson.events){
+      eventsCollection.addNode(item)
+      console.log(">>> collected event: " + item.url);
+    }
   })
 
-  api.createPages(({ createPage }) => {
+  api.createPages(async ({ graphql, createPage }) => {
     // Use the Pages API here: https://gridsome.org/docs/pages-api/
+    const { data } = await graphql(`{
+      allEvent {
+        edges {
+          node {
+            category
+            type
+            url
+            title
+            performer
+            description
+            price
+            time {
+              start
+              end
+            }
+            date {
+              start
+              end
+            }
+            image
+            ticket
+            ics
+          }
+        }
+      }
+    }`)
+
+    data.allEvent.edges.forEach(({ node }) => {
+      const cat = node.category || "g";
+      createPage({
+        path: `/events/${cat}/${node.url}`,
+        component: './src/templates/Event.vue',
+        // Use context to be able to pass event data to the template
+        // (I could not figure out how to get access to the data through graphql)
+        context: node
+      })
+    })
   })
 }
---
+++ src/templates/Event.vue
@@ -0,0 +1,40 @@
+<template>
+  <Layout>
+    <event-item 
+      :event="event"
+      :atPageLevel="true"
+      class="anima__zoom"
+    />
+
+  </Layout>
+</template>
+
+
+<script>
+import EventItem from '@/components/EventItem';
+
+export default {
+  name: 'EventList',
+  components: {
+    EventItem,
+  },
+  props: {
+  },
+  data() {
+    return {
+    }
+  },
+  mounted () {
+  },
+  computed: {
+    event() {
+      return this.$context;
+    },
+  },
+  methods: {
+  }
+};
+</script>
+
+<style lang="less" scoped>
+</style>
