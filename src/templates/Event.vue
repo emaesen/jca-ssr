3ec02292a11a4d7896ae261e43d20332bd75f86b
Edward <edward.maesen@gmail.com>
2019-11-08 11:59:51 -0800

    Tweak event meta tags

--- src/templates/Event.vue
+++ src/templates/Event.vue
@@ -21,13 +21,17 @@ export default {
   },
   metaInfo () {
     return {
-      title: this.event.title + ' at Jefferson Center for the Arts in Mount Shasta, CA, USA',
+      title: this.event.title + ' at JCA Mount Shasta',
       meta: [
         { key: 'og:type', property: 'og:type', content: 'event'},
         { key: 'og:title', property: 'og:title', content: this.event.title },
-        { key: 'description', name: 'description', content: this.meta_description },
         { key: 'og:description', property: 'og:description', content: this.meta_description },
-        { key: 'og:url', property: 'og:url', content: this.meta_url}
+        { key: 'og:url', property: 'og:url', content: this.meta_url},
+        { key: 'og:image', property: 'og:image', content: this.meta_image},
+        { key: 'og:image:width', property: 'og:image:width', content: this.event.image_width},
+        { key: 'og:image:height', property: 'og:image:height', content: this.event.image_height},
+        { key: 'description', name: 'description', content: this.meta_description },
+        { key: 'twitter:image', name: 'twitter:image', content: this.meta_image},
       ],
       link: [
         { key: 'canonical', rel: 'canonical', href: this.meta_url}
@@ -46,6 +50,9 @@ export default {
     },
     meta_url() {
       return 'https://jeffersoncenterforthearts.com' + this.$router.currentRoute.path
+    },
+    meta_image() {
+      return 'https://jeffersoncenterforthearts.com/img/event/' + this.event.image
     },
     meta_description() {
       return "A " + (this.event.category || this.event.type) + " event at Jefferson Center for the Arts in Mount Shasta on " + this.event.date.start;
