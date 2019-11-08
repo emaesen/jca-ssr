11e8777400d4bcfa4678e630525372232cb77fa3
Edward <edward.maesen@gmail.com>
2019-11-07 17:02:37 -0800

    Switch from thundertix to eventbrite tickets

--- src/components/EventTicket.vue
+++ src/components/EventTicket.vue
@@ -9,11 +9,11 @@
       v-if="showModal"
       @close="closeModal"
     >
-      <iframe class="iframe-class" width="100%" :height="frameHeight" src="https://jeffersoncenterforthearts.thundertix.com/events/158781" frameborder="0" scrolling="auto" marginheight="0" marginwidth="0" allowtransparency="true" ></iframe>
+      <iframe class="iframe-class" width="100%" :height="frameHeight" :src="href" frameborder="0" scrolling="auto" marginheight="0" marginwidth="0" allowtransparency="true" ></iframe>
 
       <template v-slot:header>
-        <div class="legal">JCA uses ThunderTix as their online ticket sales platform. By clicking below to purchase, you acknowledge that your information will be transferred to ThunderTix for processing.<br/>
-        <link-outbound to="https://www.thundertix.com/privacy-policy/">About ThunderTix's privacy practices.</link-outbound></div>
+        <div class="legal">JCA uses Eventbrite as their online ticket sales platform. By clicking below to purchase, you acknowledge that your information will be transferred to Eventbrite for processing.<br/>
+        <link-outbound to="https://www.eventbrite.com/privacypolicy">About Eventbrite's privacy practices.</link-outbound></div>
       </template>
     </modal>
   </div>
@@ -34,19 +34,19 @@ export default {
     Modal,
   },
   props: {
-    id: {
+    ticketUrl: {
       type: String
     },
   },
   data() {
     return {
       showModal: false,
-      baseUrl: "https://jeffersoncenterforthearts.thundertix.com/events/",
+      baseUrl: "//eventbrite.com/tickets-external?eid=[ticket]&ref=etckt",
     }
   },
   computed: {
     href() {
-      return this.baseUrl + this.id;
+      return this.baseUrl.replace("[ticket]", this.ticketUrl);
     },
     frameHeight() {
       return (this.windowHeight - 150) + "px";
@@ -75,6 +75,12 @@ button {
   color: #b22222;
   a {
     color: #3b5998;
+  }
+}
+@media all and (max-width: 650px) {
+  .legal {
+    margin-left: 20px;
+    font-size: 60%;
   }
 }
 </style>
