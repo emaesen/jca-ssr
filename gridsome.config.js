fc91f9d6156a6e4959a0320cc193fc590ee3221b
Edward <edward.maesen@gmail.com>
2019-11-14 16:51:29 -0800

    Bump version

--- gridsome.config.js
+++ gridsome.config.js
@@ -9,7 +9,7 @@ module.exports = {
   siteUrl: 'https://jeffersoncenterforthearts.com/',
   titleTemplate: '%s',
   metadata: {
-    version: 'V1.4.2',
+    version: 'V1.4.4',
   },
   icon: {
     favicon: './static/img/icons/favicon.png',
