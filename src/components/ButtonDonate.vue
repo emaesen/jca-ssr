3ece4e251be0f3d7a2525f2a87cfdec3cbda5352
Edward <edward.maesen@gmail.com>
2021-03-22 16:56:49 -0700

    add donate button and section

---
+++ src/components/ButtonDonate.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <button class="action" @click="openModal" title="click to view donate information">Donate</button>
+
+    <modal
+      v-if="showModal"
+      @close="closeModal"
+    >
+      <donate-section/>
+    </modal>
+
+  </div>
+</template>
+
+<script>
+import Modal from '@/components/Modal.vue';
+import DonateSection from '@/components/DonateSection.vue';
+
+
+export default {
+  name: 'ButtonDonate',
+  components: {
+    Modal,
+    DonateSection,
+  },
+  props: {
+    event: {
+      type: Object
+    },
+  },
+  data() {
+    return {
+      showModal: false,
+    }
+  },
+  computed: {
+  },
+  methods: {
+    openModal() {
+      this.showModal = true;
+    },
+    closeModal() {
+      this.showModal = false;
+    },
+
+  },
+}
+</script>
+
+<style lang="less" scoped>
+button {
+  margin: .5em 1em;
+  border: 1px solid rgb(255,222,124);
+  box-shadow: 0 0 5px rgb(255,222,124);
+  color: rgb(255,222,124);
+}
+</style>
---
+++ src/components/DonateSection.vue
@@ -0,0 +1,26 @@
+<template>
+  <section>
+      <h2>Donate:</h2>
+      <p>
+        TAX-DEDUCTIBLE DONATIONS can be made to Jefferson Center for the Arts (JCA) via <link-outbound to="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=D5J6TL8C94YE2&source=url">PayPal</link-outbound>.<br>
+        <br>
+        The JCA is a 501(c)(3) nonprofit organization which relies on community support to continue offering its art education programs and performances.<br>
+        <br>
+        Thank you! 
+      </p>
+    </section>
+
+</template>
+
+
+<script>
+import LinkOutbound from '@/components/LinkOutbound.vue';
+
+export default {
+  name: 'DonateSection',
+  mixins: [],
+  components: {
+    LinkOutbound,
+  },
+}
+</script>
