6ad42db2fabd2a48e91bdc070722a0161dc42421
emaesen <edward.maesen@gmail.com>
2024-11-04 11:07:11 -0800

    add PO box info

--- src/components/DonateSection.vue
+++ src/components/DonateSection.vue
@@ -2,10 +2,11 @@
   <section>
       <h2>Donate:</h2>
       <p>
-        TAX-DEDUCTIBLE DONATIONS can be made to Jefferson Center for the Arts (JCA) via <link-outbound to="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=D5J6TL8C94YE2&source=url">PayPal</link-outbound>.<br>
-        <br>
         The JCA is a 501(c)(3) nonprofit organization (EIN 85–0684251) which relies on community support to continue offering its art education programs and performances.<br>
         <br>
+        TAX-DEDUCTIBLE DONATIONS can be made to Jefferson Center for the Arts (JCA) via <link-outbound to="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=D5J6TL8C94YE2&source=url">PayPal</link-outbound>,<br>or by check to:<br>JCA<br>PO BOX 714<br>MT SHASTA, CA 96067<br>
+        <br>
+        
         Thank you! 
       </p>
     </section>
