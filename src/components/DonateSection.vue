7bb403451e1e52a860f8ce9e40e6734cb6e949a1
emaesen <edward.maesen@gmail.com>
2024-12-03 11:39:51 -0800

    add zeffy as donation link

--- src/components/DonateSection.vue
+++ src/components/DonateSection.vue
@@ -4,9 +4,14 @@
       <p>
         The JCA is a 501(c)(3) nonprofit organization (EIN 85–0684251) which relies on community support to continue offering its art education programs and performances.<br>
         <br>
-        TAX-DEDUCTIBLE DONATIONS can be made to Jefferson Center for the Arts (JCA) via <link-outbound to="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=D5J6TL8C94YE2&source=url">PayPal</link-outbound>,<br>or by check to:<br>JCA<br>PO BOX 714<br>MT SHASTA, CA 96067<br>
+        TAX-DEDUCTIBLE DONATIONS can be made to<br>
+        Jefferson Center for the Arts (JCA)<br>
+        via <link-outbound to="https://www.zeffy.com/en-US/donation-form/jca-donation">Zeffy</link-outbound> or <link-outbound to="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=D5J6TL8C94YE2&source=url">PayPal</link-outbound>,<br>
+        or you may send a check to:<br>
+        JCA<br>
+        PO BOX 714<br>
+        MT SHASTA, CA 96067<br>
         <br>
-        
         Thank you! 
       </p>
     </section>
