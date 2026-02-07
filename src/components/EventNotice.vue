c51ca83b62b656a29788f5814869f692327ee3fa
emaesen <edward.maesen@gmail.com>
2026-02-07 11:01:26 -0800

    tweak event notice

--- src/components/EventNotice.vue
+++ src/components/EventNotice.vue
@@ -3,13 +3,14 @@
     <!--
     JCA artists often have to travel quite a distance to play in Mount Shasta.<br>If tickets aren’t purchased or reserved, we are unfortunately forced to cancel.<br>So if you are interested in coming to a show,<br><span class="emph">drop us an email or buy your tix in advance</span><br>so we can assure the artists of a nice good crowd.<br>It would help us out a-lot!
     -->
-    We've been off to an AMAZING start in 2026 with two sold out shows Jason Dea West and Gareth Pearson.<br>
+    We've been off to an AMAZING start in 2026 with two sold out shows Jason Dea West and Gareth Pearson.
+    <br><span class="xbr"></span>
     We have a new outdoor fire pit and some yummy new offerings at the bar. Come see us!
     <br><span class="xbr"></span>
     <span class="emph">RESERVATIONS AVAILABLE FOR ALL SHOWS</span>
     <br><span class="xbr"></span>
-    If you know you want to come (but are hesitant to buy an advance ticket online) reserve a ticket for the adv price via email and pay at the venue.
-    Given that shows might sell out, we want to make sure you can get yourself a ticket!
+    <span class="deemph">If you know you want to come (but are hesitant to buy an advance ticket online) reserve a ticket for the adv price via email and pay at the venue.
+    Given that shows might sell out, we want to make sure you can get yourself a ticket!</span>
     <br><span class="xbr"></span>
     Hope to see you soon!
   </div>
@@ -22,5 +23,6 @@
   padding: 1em;
   border-radius: 10px;
   margin-bottom:1em;
+  font-size:90%;
 }
 </style>
